03a0cdbf2c30ddc774f1a615454504bb3536ee5f
Mark Menard <mark@mjm.net>
2011-05-22 09:18:59 -0400

    Refactor method to a one-liner in DatePropertyDefinition.

--- lib/doc_wrapper/date_property_definition.rb
+++ lib/doc_wrapper/date_property_definition.rb
@@ -3,12 +3,7 @@ require 'date'
 module DocWrapper
   class DatePropertyDefinition < InnerHtmlPropertyDefinition
     def transform (result)
-      if block
-        result = block.call(result)
-      else
-        result = result.blank? ? nil : (options[:parser] ? options[:parser].call(result) : Date.parse(result))
-      end
-      result
+      block ? block.call(result) : (result.blank? ? nil : (options[:parser] ? options[:parser].call(result) : Date.parse(result)))
     end
   end
 end
