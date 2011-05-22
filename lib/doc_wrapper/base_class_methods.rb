2e4a17e06c3cebdd16754e28ad00b31e27b98130
Mark Menard <mark@mjm.net>
2011-05-22 09:11:43 -0400

    Change has_one and has_many class methods to use new property classes.

--- lib/doc_wrapper/base_class_methods.rb
+++ lib/doc_wrapper/base_class_methods.rb
@@ -9,29 +9,26 @@ module DocWrapper
     end
 
     def multi_property (property_name, selectors, options = {}, &block)
-      raise "Multi properties require a block" if block.nil?
+      raise "Multi-properties require a block" if block.nil?
       add_property_definition(property_name, MultiPropertyDefinition.new(property_name, selectors, initialize_options(options), block))
     end
 
     def has_many (property_name, selector, klass, options = {})
-      options = initialize_options(options)
+      wrapper = HasManyPropertyDefinition.new(property_name, selector, klass, initialize_options(options))
       define_method property_name do
-        get_has_many( property_name, selector, klass, options)
+        wrapper.property(documents)
       end
     end
 
     def has_one (property_name, selector, klass, options = {})
-      options = initialize_options(options)
+      wrapper = HasOnePropertyDefinition.new(property_name, selector, klass, initialize_options(options))
       define_method(property_name) do
-        get_has_one(property_name, selector, klass, options)
+        wrapper.property(documents)
       end
     end
     
     def namespaces (namespaces)
       @namespaces = namespaces
-      # define_method(:namespaces) do
-      #   namespaces
-      # end
     end
     
     ##################
@@ -40,27 +37,26 @@ module DocWrapper
 
     def add_property_definition (property_name, wrapper)
       add_property_name(property_name)
-      add_property_wrapper(property_name, wrapper)
-      add_property_accessor(property_name)
+      add_property_accessor(property_name, wrapper)
     end
 
     # Add a property name to the singleton property_names attribute.
     def add_property_name (property_name)
-      # Add the property name to the property_names collection.
       self.property_names << property_name
     end
 
-    # Add a property wrapper to the property_definitions Hash.
-    def add_property_wrapper (property_name, wrapper)
-      self.property_definitions[property_name] = wrapper
+    def add_property_accessor (property_name, wrapper)
+      define_method(property_name) do
+        wrapper.property(documents)
+      end
     end
 
-    def add_property_accessor (property_name)
-      class_eval <<-END
-        def #{property_name.to_s}
-          property_definitions[:#{property_name.to_s}].property(documents)
-        end
-      END
+    def build_property_definition (property_name, type, selector, options, block)
+      DocWrapper.const_get("#{camelize(type.to_s)}PropertyDefinition").new(property_name, type, selector, initialize_options(options), block)
+    end
+
+    def camelize (string)
+      string.gsub(/\/(.?)/) { "::#{$1.upcase}" }.gsub(/(?:^|_)(.)/) { $1.upcase }
     end
 
     # Set default options that all properties need.
@@ -71,15 +67,6 @@ module DocWrapper
       options = { :document => 1 }.merge(options)
       options[:namespaces] = @namespaces if @namespaces
       options
-    end
-
-    
-    def build_property_definition (property_name, type, selector, options, block)
-      DocWrapper.const_get("#{camelize(type.to_s)}PropertyDefinition").new(property_name, type, selector, initialize_options(options), block)
-    end
-
-    def camelize (string)
-      string.gsub(/\/(.?)/) { "::#{$1.upcase}" }.gsub(/(?:^|_)(.)/) { $1.upcase }
     end
     
   end
